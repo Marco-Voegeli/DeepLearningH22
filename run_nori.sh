5d5558bd8b3ff549e1b853f4efcb5ff10084de17
Marco Voegeli <marvoegeli@eu-login-40.euler.ethz.ch>
2022-12-22 16:21:32 +0100

    euler script

---
+++ run_nori.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --cpus-per-task=36
+#SBATCH --time=0-08:00:00
+#SBATCH --mem-per-cpu=1G
+#SBATCH --job-name=letr
+#SBATCH --output=letr.out
+
+echo "Running LETR with $SLURM_NTASKS tasks, $SLURM_CPUS_PER_TASK cores per task, $SLURM_MEM_PER_CPU memory per core"
+
+
+python3 letr_script.py
+
+
+echo "Done rendering"
+
+exit 0
+
