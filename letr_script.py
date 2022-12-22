3e92870a06b49acb9e34a242d7fb3b3a7da5fe59
AlexisTabin <atabin@student.ethz.ch>
2022-12-22 18:39:26 +0100

    Implement script to generate demo images

--- letr_script.py
+++ letr_script.py
@@ -79,80 +79,71 @@ class Resize(object):
         size = self.sizes
         return resize(img, size, self.max_size)
 
-# obtain checkpoints
-checkpoint = torch.load('LETR/exp/res101_stage2_focal/checkpoints/checkpoint0024.pth', map_location='cpu')
+def analyse_image_letr(img_path, img_name):
+    LETR_PATH = 'wireframe_dataset/letr/'
+    # obtain checkpoints
+    checkpoint = torch.load('LETR/exp/res101_stage2_focal/checkpoints/checkpoint0024.pth', map_location='cpu')
 
-# load model
-args = checkpoint['args']
-print(type(args))
-args.device = torch.device('cpu')
-print(args.device)
-model, _, postprocessors = build_model(args)
-model.load_state_dict(checkpoint['model'])
-model.eval()
+    # load model
+    args = checkpoint['args']
+    args.device = torch.device('cpu')
+    model, _, postprocessors = build_model(args)
+    model.load_state_dict(checkpoint['model'])
+    model.eval()
 
-# load image
-raw_img = plt.imread('LETR/figures/demo.png')
-h, w = raw_img.shape[0], raw_img.shape[1]
-orig_size = torch.as_tensor([int(h), int(w)])
+    # load image
+    raw_img = plt.imread(img_path + img_name)
+    h, w = raw_img.shape[0], raw_img.shape[1]
+    orig_size = torch.as_tensor([int(h), int(w)])
 
-# normalize image
-test_size = 1100
-normalize = Compose([
-        ToTensor(),
-        Normalize([0.538, 0.494, 0.453], [0.257, 0.263, 0.273]),
-        Resize([test_size]),
-    ])
-img = normalize(raw_img)
-inputs = nested_tensor_from_tensor_list([img])
-plt.axis('off')
-plt.imshow(raw_img)
+    # normalize image
+    test_size = 1100
+    normalize = Compose([
+            ToTensor(),
+            Normalize([0.538, 0.494, 0.453], [0.257, 0.263, 0.273]),
+            Resize([test_size]),
+        ])
+    img = normalize(raw_img)
+    inputs = nested_tensor_from_tensor_list([img])
+    # plt.axis('off')
+    # plt.imshow(raw_img)
 
-outputs = model(inputs)[0]
+    outputs = model(inputs)[0]
 
-out_logits, out_line = outputs['pred_logits'], outputs['pred_lines']
-prob = F.softmax(out_logits, -1)
-scores, labels = prob[..., :-1].max(-1)
-img_h, img_w = orig_size.unbind(0)
-scale_fct = torch.unsqueeze(torch.stack([img_w, img_h, img_w, img_h], dim=0), dim=0)
-lines = out_line * scale_fct[:, None, :]
-lines = lines.view(1000, 2, 2)
-lines = lines.flip([-1])# this is yxyx format
-scores = scores.detach().numpy()
-keep = scores >= 0.7
-keep = keep.squeeze()
-lines = lines[keep]
-lines = lines.reshape(lines.shape[0], -1)
+    out_logits, out_line = outputs['pred_logits'], outputs['pred_lines']
+    prob = F.softmax(out_logits, -1)
+    scores, labels = prob[..., :-1].max(-1)
+    img_h, img_w = orig_size.unbind(0)
+    scale_fct = torch.unsqueeze(torch.stack([img_w, img_h, img_w, img_h], dim=0), dim=0)
+    lines = out_line * scale_fct[:, None, :]
+    lines = lines.view(1000, 2, 2)
+    lines = lines.flip([-1])# this is yxyx format
+    scores = scores.detach().numpy()
+    keep = scores >= 0.7
+    keep = keep.squeeze()
+    lines = lines[keep]
+    lines = lines.reshape(lines.shape[0], -1)
 
+    fig = plt.figure()
+    plt.imshow(raw_img)
+    for tp_id, line in enumerate(lines):
+        y1, x1, y2, x2 = line # this is yxyx
+        y1 = y1.detach().numpy()
+        x1 = x1.detach().numpy()
+        y2 = y2.detach().numpy()
+        x2 = x2.detach().numpy()
+        p1 = (x1, y1)
+        p2 = (x2, y2)
+        
+        tmp0 = [p1[0], p2[0]]
+        tmp1 = [p1[1], p2[1]]
 
-fig = plt.figure()
-plt.imshow(raw_img)
-for tp_id, line in enumerate(lines):
-    y1, x1, y2, x2 = line # this is yxyx
-    y1 = y1.detach().numpy()
-    x1 = x1.detach().numpy()
-    y2 = y2.detach().numpy()
-    x2 = x2.detach().numpy()
-    p1 = (x1, y1)
-    p2 = (x2, y2)
-    print("x1 : ", x1)
-    print("x2 : ", x2)
-    print("y1 : ", y1)
-    print("y2 : ", y2)
-    
-    tmp0 = [p1[0], p2[0]]
-    tmp1 = [p1[1], p2[1]]
+        plt.plot([p1[0], p2[0]], [p1[1], p2[1]], linewidth=1.5, color='darkorange', zorder=1)
+    plt.axis('off')
 
-    print("tmp0 : ", tmp0)
-    print("tmp1 : ", tmp1)
-    
-    print("type(tmp0) : ", type(tmp0))
-    print("type(x1) : ", type(x1))
-    print("type(y1) : ", type(y1))
-    plt.plot([p1[0], p2[0]], [p1[1], p2[1]], linewidth=1.5, color='darkorange', zorder=1)
-plt.axis('off')
+    analyse_image_loc = LETR_PATH + img_name
 
+    plt.savefig(analyse_image_loc, dpi=300, bbox_inches='tight', pad_inches = 0)
+    plt.close(fig)
 
-#plt.savefig("../figures/demo_result_youpi.png", dpi=300, bbox_inches='tight', pad_inches = 0)
-#plt.close(fig)
-plt.show()
+    return plt.imread(analyse_image_loc)
---
+++ plot_demo.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from random import shuffle
+import pytlsd
+from skimage.transform import pyramid_reduce
+import pickle
+import cv2
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+from IPython.display import display
+from PIL import Image
+from letr_script import analyse_image_letr
+
+NOTDEF = -1024.0
+IMG_PATH = 'wireframe_dataset/v1.1/train/'
+POINT_LINES_PATH = 'wireframe_dataset/pointlines/'
+TRAIN_IMG_NAMES_PATH = 'wireframe_dataset/v1.1/train.txt'
+
+def get_thresholded_grad(resized_img):
+    modgrad = np.full(resized_img.shape, NOTDEF, np.float64)
+    anglegrad = np.full(resized_img.shape, NOTDEF, np.float64)
+
+    # A B
+    # C D
+    A, B, C, D = resized_img[:-1, :-1], resized_img[:-1, 1:], resized_img[1:, :-1], resized_img[1:, 1:]
+    gx = B + D - (A + C)  # horizontal difference
+    gy = C + D - (A + B)  # vertical difference
+
+    threshold = 5.2262518595055063
+    modgrad[:-1, :-1] = 0.5 * np.sqrt(gx ** 2 + gy ** 2)
+    anglegrad[:-1, :-1] = np.arctan2(gx, -gy)
+    anglegrad[modgrad <= threshold] = NOTDEF
+    return gx, gy, modgrad, anglegrad
+
+def analyze_image_pytlsd(image_path='pytlsd/resources/ai_001_001.frame.0000.color.jpg'):
+    gray = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    flt_img = gray.astype(np.float64)
+
+    scale_down = 0.8
+    resized_img = pyramid_reduce(flt_img, 1 / scale_down, 0.6)
+
+    # Get image gradients
+    gx, gy, gradnorm, gradangle = get_thresholded_grad(resized_img)
+
+    segments = pytlsd.lsd(resized_img, 1.0, gradnorm=gradnorm, gradangle=gradangle)
+    segments /= scale_down
+
+    # plt.title("Gradient norm")
+    # plt.imshow(gradnorm[:-1, :-1])
+    # plt.colorbar()
+    # plt.figure()
+    # gradangle[gradangle == NOTDEF] = -5
+    # plt.title("Thresholded gradient angle")
+    # plt.imshow(gradangle[:-1, :-1])
+    # plt.colorbar()
+
+    img_color = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+    for segment in segments:
+        cv2.line(img_color, (int(segment[0]), int(segment[1])), (int(segment[2]), int(segment[3])), (0, 255, 0))
+
+    return cv2.cvtColor(img_color, cv2.COLOR_BGR2RGB), segments, gradnorm, gradangle
+
+# Load wireframe images
+
+def load_img_points(idx):
+    filename = POINT_LINES_PATH + f'{idx}.pkl'
+    infile = open(filename,'rb')
+    new_dict = pickle.load(infile)
+    infile.close()
+
+    im = new_dict['img']
+    lines = new_dict['lines']
+    points = new_dict['points']
+
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    return im, lines, points
+
+# Show wireframe images without and with lines
+
+def print_original_and_image_with_line(id):
+    im, lines, points = load_img_points(id)
+    imb_before = im.copy()
+    for idx, (i, j) in enumerate(lines, start=0):
+     x1, y1 = points[i]
+     x2, y2 = points[j]
+     cv2.line(im, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2, cv2.LINE_8)
+
+    pytlsd_im = analyze_image_pytlsd(IMG_PATH + f'{id}.jpg')[0]
+
+    letr_im = analyse_image_letr(IMG_PATH, f'{id}.jpg')
+
+    # Plot images
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(20, 20))
+
+    axes = [ax1, ax2, ax3]
+    for ax in axes:
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    ax1.imshow(imb_before)
+    ax1.set_title('Original Image')
+
+    ax2.imshow(im)
+    ax2.set_title('with ground truth lines')
+
+    ax3.imshow(pytlsd_im)
+    ax3.set_title('with PyTSLD lines')
+
+    ax4.imshow(letr_im)
+    ax4.set_title('with LETR lines')
+    
+    plt.savefig(f'wireframe_dataset/demo_{id}.png')
+
+with open(TRAIN_IMG_NAMES_PATH) as file:
+    # Not really efficent but does the job
+    lines = [line.rstrip() for line in file][:10]
+    shuffle(lines)
+    ids = [line.split('.')[0] for line in lines]
+    for id in ids:
+        print_original_and_image_with_line(id)
